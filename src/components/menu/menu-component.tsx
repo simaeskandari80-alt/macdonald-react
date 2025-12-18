bbf94914a34d96c849b3edeaee6605147a837feb
simaeskandari <simaeskandari80@gmail.com>
2025-12-18 15:53:14 +0330

    feat:creat header menu and product menu

---
+++ src/components/menu/menu-component.tsx
@@ -0,0 +1,13 @@
+import HeaderMenuComponent from "../header-menu/header-menu-component";
+
+function MenuComponent() {
+	return (
+		<div className="bg-secondary py-6 px-4 w-full md:w-3/4">
+			<div className="">
+				<HeaderMenuComponent />
+			</div>
+		</div>
+	);
+}
+
+export default MenuComponent;
---
+++ src/components/recipt/recipt-component.tsx
@@ -0,0 +1,3 @@
+export default function ReciptComponent() {
+	return <div className="bg-secondary w-full md:w-1/4">recipt</div>;
+}
